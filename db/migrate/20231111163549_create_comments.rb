7ad1562ac09bc7a21a3e6059312cb870887758ff
JenMcD-star <jenniferannvancura@gmail.com>
2023-11-11 15:01:53 -0600

    comments with partials

--- db/migrate/20231111163549_create_comments.rb
+++ db/migrate/20231111163549_create_comments.rb
@@ -1,6 +1,5 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    drop_table :comments
     create_table :comments do |t|
       t.text :content
       t.string :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_172011) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_165137) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.string "name"
